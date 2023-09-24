a9c1552482275ca22485a2171d8b50f7572bbaec
Ryan Kenney <ryan@ryankenney.info>
2023-09-24 06:26:13 -0700

    Added basic persistence of images to sqlite

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, send_from_directory, Response
+from flask import Flask, jsonify, send_from_directory, request, Response
 import cv2
 import numpy as np
 from datetime import datetime
+import sqlite3
 
 app = Flask(__name__, static_folder='menu-app/build')
 
@@ -15,6 +16,21 @@ historical_frames = {
     "one_week": {"frame": None, "timestamp": None},
     "one_month": {"frame": None, "timestamp": None},
 }
+
+# Initialize SQLite Database
+def init_db():
+    conn = sqlite3.connect("images.db")
+    cursor = conn.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS images (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        timestamp TEXT,
+        image BLOB
+    );
+    """)
+    conn.commit()
+    conn.close()
 
 def update_historical_frames(frame):
     now = datetime.now()
@@ -92,6 +108,7 @@ def generate_grid():
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
+
 @app.route('/')
 def index():
     return send_from_directory('/app/menu-app/build', 'index.html')
@@ -109,5 +126,65 @@ def video_grid():
     return Response(generate_grid(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+@app.route('/save_reference', methods=['POST'])
+def save_reference():
+    try:
+        name = request.form.get("name")  # Getting the 'name' field from POST request
+        
+        if not name:
+            return jsonify({"status": "error", "message": "Name is required"})
+
+        # Check if the webcam is opened correctly
+        if not camera.isOpened():
+            return jsonify({"status": "error", "message": "Could not open webcam"})
+
+        # Capture a single frame
+        ret, frame = camera.read()
+
+        if not ret:
+            return jsonify({"status": "error", "message": "Failed to capture image"})
+
+        # Generate timestamp
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        # Convert image to binary data
+        _, img_encoded = cv2.imencode('.jpg', frame)
+        img_binary = img_encoded.tobytes()
+
+        # Insert name, timestamp, and image into the SQLite database
+        conn = sqlite3.connect("images.db")
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO images (name, timestamp, image) VALUES (?, ?, ?)", (name, timestamp, img_binary))
+        conn.commit()
+        conn.close()
+
+        return jsonify({"status": "success"})
+        
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)})
+
+@app.route('/get_references', methods=['GET'])
+def get_references():
+    try:
+        conn = sqlite3.connect("images.db")
+        cursor = conn.cursor()
+        cursor.execute("SELECT id, name, timestamp FROM images")
+        
+        images = []
+        for row in cursor.fetchall():
+            record_id, name, timestamp = row
+            images.append({
+                'id': record_id,
+                'name': name,
+                'timestamp': timestamp
+            })
+        
+        conn.close()
+        return jsonify(images)
+    
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)})
+
 if __name__ == '__main__':
+    init_db()
     app.run(host='0.0.0.0', port='5000')
